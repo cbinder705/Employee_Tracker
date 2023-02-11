66f96d6fbbc7900c91031c319c705e94072ed7b7
Cory Bee <cbinder705@gmail.com>
2023-02-11 15:14:04 -0500

    added seeds to seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,17 @@
+INSERT INTO department (name)
+VALUES 
+("Sales"),
+("Legal")
+("Finance");
+
+INSERT INTO role (department_id, title, salary)
+VALUES (001, "Manager", 80000),
+(001,"Lead Sales",50000 ),
+(002, "Lawyer", 100000),
+(003, "Payroll", 75000)
+
+INSERT INTO employee (role_id, first_name, last_name)
+VALUES (001, "Jesse", "Pinkman")
+(001, "Tim", "Burton")
+(002, "Saul", "Goodman")
+(003, "Walter", "White")
