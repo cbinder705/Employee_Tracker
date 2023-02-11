b4fc70a0560447ecea3d30f5a4c6e528ef66cefe
Cory Bee <cbinder705@gmail.com>
2023-02-11 15:21:28 -0500

    created role table in schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -4,10 +4,15 @@ CREATE DATABASE myEmployees_db;
 use myEmployees_db;
 
 CREATE TABLE department (
-  id INT NOT NULL AUTO INCREMENT PRIMARY KEY,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR (30) NOT NULL
 );
 
 CREATE TABLE role (
-  
-)
+  id INT NOT NUTLL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR (30) NOT NULL,
+  salary DECIMAL, NOT NULL,
+  department_id INT,
+  FOREIGN KEY (department_id)
+  REFERENCES department(id)
+);
