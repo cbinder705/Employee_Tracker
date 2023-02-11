4615512a76d5fe4121d4e308ad02c881ee5d7127
Cory Bee <cbinder705@gmail.com>
2023-02-11 17:01:27 -0500

    added create employee function to server, updated schema and seeds

--- db/schema.sql
+++ db/schema.sql
@@ -9,9 +9,9 @@ CREATE TABLE department (
 );
 
 CREATE TABLE role (
-  id INT NOT NUTLL AUTO_INCREMENT PRIMARY KEY,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR (30) NOT NULL,
-  salary DECIMAL, NOT NULL,
+  salary DECIMAL(10, 2) NOT NULL,
   department_id INT,
   FOREIGN KEY (department_id)
   REFERENCES department(id)
@@ -22,9 +22,9 @@ CREATE TABLE employee(
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR (30) NOT NULL,
   role_id INT,
-  FOREIGN KEY (role_id),
-  REFERENCES role(id),
-  manager_id INT,
-  FOREIGN KEY (manager_id)
-  REFERENCES employee(id)
-);
+  FOREIGN KEY (role_id)
+REFERENCES role(id),
+manager_id INT,
+FOREIGN KEY (manager_id)
+REFERENCES employee(id)
+)
